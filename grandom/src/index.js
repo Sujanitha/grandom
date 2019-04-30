09b1e202cd91de32e9d73a4b2be2062f28a82f49
Mike <mcioce@uncc.edu>
2019-04-30 10:56:33 -0400

    adding firebase app initialization and database link statements to index.js

--- grandom/src/index.js
+++ grandom/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as Firebase from 'firebase';
+//firebase configuration
+var config = {
+    apiKey: "AIzaSyDrtMstu-qtUgZ80cuuvBDwkf902q0jBys",
+    authDomain: "grandom-team2.firebaseapp.com",
+    databaseURL: "https://grandom-team2.firebaseio.com",
+    projectId: "grandom-team2",
+    storageBucket: "grandom-team2.appspot.com",
+    messagingSenderId: "721988283958"
+};
+Firebase.initializeApp(config);
+//database connection
+var database = Firebase.database();
+// ReactDOM.render(, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
