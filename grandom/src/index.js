6fce26aa695b6555a2c8b2a4f5816f96e308ccfb
Mike <mcioce@uncc.edu>
2019-04-30 16:07:44 -0400

    added new configuration for new firebase project

--- grandom/src/index.js
+++ grandom/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as firebase from 'firebase';
 //firebase configuration
+var config = {
+    apiKey: "AIzaSyCN7ZgwYt_mFOaTOWTjKVOosm0JKtWTWeY",
+    authDomain: "grandom-2.firebaseapp.com",
+    databaseURL: "https://grandom-2.firebaseio.com",
+    projectId: "grandom-2",
+    storageBucket: "grandom-2.appspot.com",
+    messagingSenderId: "255878573"
+};
+firebase.initializeApp(config);
 
 //database connection
-// var database = firebase.database();
+var database = firebase.database();
 // ReactDOM.render(, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
